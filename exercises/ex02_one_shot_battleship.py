303bfcfc6af8ff84da183a712c6c25cd51cfc24f
Stephanie Riano <rianos@email.unc.edu>
2024-02-02 15:03:46 -0500

    updating changes

---
+++ exercises/ex02_one_shot_battleship.py
@@ -0,0 +1,10 @@
+"""EX02 - One Shot Battleship"""
+
+__author__ = "730403386"
+
+grid_size: int = 4
+secret_row: int = 3
+secret_column: int = 2
+
+row_input: str = input("Guess a row: ")
+row_number: int = int(row_input)
