e1ca9295b753135b356bf2b34b59437b2c708ae5
Stephanie Riano <rianos@email.unc.edu>
2024-02-19 18:45:31 -0500

    officially finished ex03

--- exercises/ex03_functional_battleship.py
+++ exercises/ex03_functional_battleship.py
@@ -17,8 +17,8 @@ def input_guess(grid_size: int, row_or_col: str) -> int:
         while row_guess < 1:
             row_input = str(input("The grid is only " + str(grid_size) + " by " + str(grid_size) + ". Try again: "))
             row_guess = int(row_input)
-        return(row_guess)
-    if row_or_col == "column":
+        return row_guess
+    else:
         column_input = str(input("Guess a column: "))
         column_guess = int(column_input)
         while column_guess > grid_size:
@@ -27,10 +27,10 @@ def input_guess(grid_size: int, row_or_col: str) -> int:
         while column_guess < 1:
             column_input = str(input("The grid is only " + str(grid_size) + " by " + str(grid_size) + ". Try again: "))
             column_guess = int(column_input)
-        return(column_guess)
+        return column_guess
     
 
-def print_grid(grid_size: int,row_guess: int,column_guess: int,check: bool) -> None:
+def print_grid(grid_size: int, row_guess: int, column_guess: int, check: bool) -> None:
     """Guesses are being compared to secret row or column and boxes are printed."""
     BLUE_BOX: str = "\U0001F7E6"
     RED_BOX: str = "\U0001F7E5"
@@ -43,7 +43,7 @@ def print_grid(grid_size: int,row_guess: int,column_guess: int,check: bool) -> N
         if row_guess == row_counter:
             while column_counter <= grid_size:
                 if column_guess == column_counter:
-                    if check == True:
+                    if check is True:
                         row_str += RED_BOX
                     else: 
                         row_str += WHITE_BOX
@@ -57,7 +57,6 @@ def print_grid(grid_size: int,row_guess: int,column_guess: int,check: bool) -> N
         row_counter += 1 
         print(row_str)
         row_str = ""
-    
 
 
 def correct_guess(secret_row: int, secret_col: int, row_guess: int, col_guess: int) -> bool:
@@ -79,7 +78,7 @@ def main(grid_size: int, secret_row: int, secret_col: int) -> None:
         main_col_guess = input_guess(grid_size, row_or_col)
         main_check = correct_guess(secret_row, secret_col, main_row_guess, main_col_guess)
         print_grid(grid_size, main_row_guess, main_col_guess, main_check)
-        if main_check == True:
+        if main_check is True:
             print("Hit!")
             print("You won in " + str(turn_counter) + "/5 turns!")
             turn_counter = 6
